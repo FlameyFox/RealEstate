ab619824c11e118ead9a5dfec7f1f2c64875d96b
FlameyFox <flameblader95@live.dk>
2022-09-30 22:01:28 +0200

    footer

---
+++ components/ui/theFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>Footer</footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  display: flex;
+  align-items: center;
+}
+</style>
