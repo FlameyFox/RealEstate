0737f64a19c9d184ae43cd322c46a90c43306469
FlameyFox <flameblader95@live.dk>
2022-11-18 20:15:19 +0100

    preload font maybe i dunno

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'preload', href: '~/assets/fonts/Nunito-VariableFont_wght.ttf' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
