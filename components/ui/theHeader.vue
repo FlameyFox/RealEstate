1eac8ea60db7d71c37bb237cc9633ed05ea03e32
FlameyFox <flameblader95@live.dk>
2022-11-21 19:02:23 +0100

    added simple login modal placeholder

--- components/ui/theHeader.vue
+++ components/ui/theHeader.vue
@@ -15,15 +15,40 @@
           <nuxt-link to="/blog">Blog</nuxt-link>
         </li>
         <li>
-          <nuxt-link to="#login">Login</nuxt-link>
+          <a @click="openModal()">Login</a>
         </li>
       </ul>
+    </div>
+    <div v-if="this.loginModal" class="login modal">
+      <span @click="openModal()">Close modal</span>
+      <h2>Login</h2>
+      <form>
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <input type="submit" title="Login" name="Login" value="Login" />
+      </form>
     </div>
   </header>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      loginModal: false,
+    }
+  },
+  methods: {
+    openModal() {
+      this.loginModal = !this.loginModal
+
+      document.body.classList.toggle('sliderActive')
+    },
+  },
+  beforeDestroy() {
+    document.body.classList.remove('sliderActive')
+  },
+}
 </script>
 
 <style lang="scss" scoped>
@@ -58,6 +83,7 @@ header {
         text-decoration: none;
         position: relative;
         transition: color 0.2s ease-in-out;
+        cursor: pointer;
 
         &:visited,
         &:active {
@@ -77,13 +103,57 @@ header {
           transform-origin: right;
         }
         &:hover {
-          color: #999;
+          color: #555;
         }
 
         &.nuxt-link-exact-active {
           &::after {
             transform: scaleX(1);
             transform-origin: left;
+          }
+        }
+      }
+    }
+  }
+  .modal {
+    position: fixed;
+    top: 50vh;
+    left: 50vw;
+    transform: translate(-50%, -50%);
+
+    background-color: #fff;
+    border-radius: 1.75rem;
+    padding: 2.75rem;
+    width: 30rem;
+    z-index: 99;
+
+    > span{
+      cursor: pointer;
+    }
+
+    h2 {
+      font-size: clamp(1.2rem, 4vw, 2rem);
+      text-align: center;
+      margin-bottom: 2rem;
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+
+      input {
+        background-color: $gray;
+        border: 0;
+        padding: 1rem 1.5rem;
+        border-radius: 1.75rem;
+        margin-bottom: 1rem;
+
+        &[type='submit'] {
+          cursor: pointer;
+          transition: all 0.3s ease-in-out;
+          &:hover {
+            background-color: $primary;
+            color: #fff;
           }
         }
       }
